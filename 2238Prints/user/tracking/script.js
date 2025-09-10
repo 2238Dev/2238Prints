8812775ae002f7030c22b3e13b7515038922f3e6
2238Dev <bogus.zima@gmail.com>
2025-09-10 17:13:43 +0200

    fix

--- 2238Prints/user/tracking/script.js
+++ 2238Prints/user/tracking/script.js
@@ -10,7 +10,7 @@ document.querySelector("#listBtn").addEventListener("click", function () {
     window.location.href = "/list";
 });
 
-const trackid = Number(document.getElementById("tid").value);
+const trackid = document.getElementById("tid").value;
 
 async function getData(trackid) {
   console.log('Start pobierania danych dla id:', trackid);
