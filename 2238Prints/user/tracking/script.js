48d4364108723e6f54981847ecea26238ab16ba2
2238Dev <bogus.zima@gmail.com>
2025-09-10 16:45:30 +0200

    update

--- 2238Prints/user/tracking/script.js
+++ 2238Prints/user/tracking/script.js
@@ -10,18 +10,36 @@ document.querySelector("#listBtn").addEventListener("click", function () {
     window.location.href = "/list";
 });
 
+async function getData() {
+  const params = new URLSearchParams(window.location.search);
+  const konkretneId = params.get('id');
+  console.log('Start pobierania danych dla id:', konkretneId);
 
+  const { data, error } = await window.supabase
+    .from('package')
+    .select('*')
+    .eq('id', konkretneId);
+
+  if (error) {
+    console.error('Błąd podczas pobierania:', error);
+    return;
+  }
+}
+
+getData()
 
 document.querySelector("#submit").addEventListener("click", function (e) {
     let trackid = Number(document.getElementById("tid").value);
     e.preventDefault()
-    switch(trackid){
-        case 1163306092025:
-            window.location.href = `/user/tracking/ui/?id=${trackid}`;
-            break
-        default:
-            document.querySelector(".info").textContent = `sorry, we don't have package: ${trackid}`
-    };
+
+    const data = await getData();
+    if (!data) return;
+
+    for (let i = 0; i < data.length; i++ ){
+        if (trackid === data[i].id){
+            window.location.href = `/user/tracking/ui/?id=${trackid}`
+        }
+    }
 })
 
 
